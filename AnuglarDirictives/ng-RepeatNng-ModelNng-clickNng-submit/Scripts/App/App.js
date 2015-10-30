43bdb0f301c7868f7c48758e0e612c9e6d1d38c3
suzauddola <suza.cse035@gmail.com>
2015-10-30 11:44:30 +0600

    user name, picture and repository information are complete using Angular controller, module, directives

---
+++ AnuglarDirictives/ng-RepeatNng-ModelNng-clickNng-submit/Scripts/App/App.js
@@ -0,0 +1,40 @@
+﻿(function () {
+
+    var app = angular.module("githubViewer", []);
+
+
+    var MainController = function ($scope, $http) {
+
+        
+
+        var onRepos = function (response) {
+            $scope.repos = response.data;
+        };
+
+        var onError = function (response) {
+            $scope.error = "Could not fetch the Data";
+        };
+
+        var onUserComplete = function (response) {
+            $scope.user = response.data;
+            $http.get($scope.user.repos_url)
+                .then(onRepos, onError);
+        };
+
+        //$http.get("https://api.github.com/users/suzauddola")
+        //  .then(onUserComplete, onError);
+
+        $scope.search = function (username) {
+            $http.get("https://api.github.com/users/" + username)
+                .then(onUserComplete, onError);
+        };
+
+        $scope.username = "angular";
+        $scope.massage = "Github Viewer";
+        $scope.repoSortOrder = "-stargazers_count";
+
+
+    };
+
+    app.controller("MainController", ["$scope", "$http", MainController]);
+}());
