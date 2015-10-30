ff70761e74b6b2afaa78116bdb7792b92b5d45cc
suzauddola <suza.cse035@gmail.com>
2015-10-30 15:10:03 +0600

    Use different kind service like $interval, $log, $anchorScroll and location. It's really emerging

--- AnuglarDirictives/ng-RepeatNng-ModelNng-clickNng-submit/Scripts/App/App.js
+++ AnuglarDirictives/ng-RepeatNng-ModelNng-clickNng-submit/Scripts/App/App.js
@@ -3,12 +3,14 @@
     var app = angular.module("githubViewer", []);
 
 
-    var MainController = function ($scope, $http) {
-
+    var mainController = function (
+        $scope, $http, $interval,
+        $log, $anchorScroll, $location) {
         
-
         var onRepos = function (response) {
             $scope.repos = response.data;
+            $location.hash("userDetaisl");
+            $anchorScroll();
         };
 
         var onError = function (response) {
@@ -24,17 +26,37 @@
         //$http.get("https://api.github.com/users/suzauddola")
         //  .then(onUserComplete, onError);
 
+
+        var decrementCountdown = function() {
+            $scope.countdown -= 1;
+            if ($scope.countdown < 1) {
+                $scope.search($scope.username);
+            }
+        };
+
+        var countdownInterval = null;
+
+        var startCountdown = function() {
+            countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+        };
+
         $scope.search = function (username) {
+            $log.info("Searching for " + username);
             $http.get("https://api.github.com/users/" + username)
                 .then(onUserComplete, onError);
+            if (countdownInterval) {
+                $interval.cancel(countdownInterval);
+                $scope.countdown = null;
+            }
         };
 
         $scope.username = "angular";
         $scope.massage = "Github Viewer";
         $scope.repoSortOrder = "-stargazers_count";
-
+        $scope.countdown = 5;
+        startCountdown();
 
     };
 
-    app.controller("MainController", ["$scope", "$http", MainController]);
+    app.controller("mainController", mainController);
 }());
